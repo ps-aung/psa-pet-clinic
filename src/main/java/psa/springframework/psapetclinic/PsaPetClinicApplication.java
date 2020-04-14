c9337d5cd9c70101ba12297db4108077e314e307
Pyae Sone Aung <pyaesone1511@icloud.com>
2020-04-14 11:15:21 +0630

    First Commit for PSA Pet Clinic

---
+++ src/main/java/psa/springframework/psapetclinic/PsaPetClinicApplication.java
@@ -0,0 +1,13 @@
+package psa.springframework.psapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsaPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/psa/springframework/psapetclinic/PsaPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package psa.springframework.psapetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsaPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
