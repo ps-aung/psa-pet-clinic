1aa71be0e5bd39a8ac1915090a4d2024f70284dd
Pyae Sone Aung <pyaesone1511@icloud.com>
2020-04-14 11:30:22 +0630

    Adding POJOs

---
+++ src/main/java/psa/springframework/psapetclinic/model/Owner.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package psa.springframework.psapetclinic.model;
+
+/**
+ * Created by pyaesoneaung on 14/04/2020
+ *
+ */
+public class Owner extends Person {
+
+}
---
+++ src/main/java/psa/springframework/psapetclinic/model/PetType.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package psa.springframework.psapetclinic.model;
+
+/**
+ * Created by pyaesoneaung on 14/04/2020
+ *
+ */
+public class PetType {
+	
+	private String name;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
