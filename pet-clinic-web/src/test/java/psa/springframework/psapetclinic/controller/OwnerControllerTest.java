ccf95fcb28814f7c64dd74f4a6f37f8e9cc774f3
Pyae Sone Aung <pyaesone1511@icloud.com>
2021-01-03 14:40:50 +0630

    fix : lombok error after updating IDE.

--- pet-clinic-web/src/test/java/psa/springframework/psapetclinic/controller/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/psa/springframework/psapetclinic/controller/OwnerControllerTest.java
@@ -15,7 +15,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
