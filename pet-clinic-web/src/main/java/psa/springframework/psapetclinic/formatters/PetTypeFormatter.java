927c60adc227ad84653f92a4ddd40ef12fcb08a6
Pyae Sone Aung <pyaesone1511@icloud.com>
2021-01-26 10:29:51 +0630

    fix : create pet not working

---
+++ pet-clinic-web/src/main/java/psa/springframework/psapetclinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,37 @@
+package psa.springframework.psapetclinic.formatters;
+
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+import psa.springframework.psapetclinic.model.PetType;
+import psa.springframework.psapetclinic.services.PetTypeService;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Collection<PetType> findPetTypes = petTypeService.findAll();
+
+        for (PetType type : findPetTypes) {
+            if (type.getName().equals(text)) {
+                return type;
+            }
+        }
+
+        throw new ParseException("type not found: " + text, 0);
+    }
+}
